84a82cbe2a713c9149b322ff5dcceddf381191ff
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-04-04 10:47:18 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import './App.css'
-import { Background } from './Components/Background'
+// App.tsx
+import { theme } from './theme/theme'; 
+import {  ThemeProvider } from '@mui/material';
+import BackgroundImage from './Components/BackgroundImage';
+import SignInForm from './Components/SignInForm';
+
+const handleGameStart = (name: string) => {
+  console.log("Spiel gestartet für:", name);
+  // Logik zum Starten des Spiels hier
+};
 
 function App() {
- 
-
   return (
-    <>
-      < Background />
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <SignInForm onGameStart={handleGameStart} />
+      <BackgroundImage />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/theme/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+
+    palette: {
+        primary: {
+            main: '#ff0000',
+        },
+        secondary: {
+            main: '#0000ff',
+        },
+    },
+});
+
+export { theme };
