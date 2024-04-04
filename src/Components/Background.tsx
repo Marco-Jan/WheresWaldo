98398b965729313d03a26563e8a800556f9488cb
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-04-04 08:38:36 +0200

    commit Project

---
+++ src/Components/Background.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Background: React.FC = () => {
+    return (
+        <>
+            <img src="src/img/wimmelCartoon.png" alt="wimmelCartoon" />
+        </>
+    );
+}
+
+export { Background };
+
---
+++ src/firebase/firebaseInit.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIOpOLaLlYpnzUyHoNCnQAhmToCRbe-kE",
+  authDomain: "where-swaldo-12064.firebaseapp.com",
+  projectId: "where-swaldo-12064",
+  storageBucket: "where-swaldo-12064.appspot.com",
+  messagingSenderId: "74243743486",
+  appId: "1:74243743486:web:a42788f7c3c9cae8facfaf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
